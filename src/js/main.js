a5b0293b0dcb04b607603513534e9f079d59fbaf
Azreil-OFD <fantom2413@gmail.com>
2022-09-27 17:40:31 +0500

    v1 готово

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 var button = document.getElementById("addButton");
-button.onclick = handleButtonClick;
+                                            // получаем кнопку
+button.onclick = handleButtonClick;         // выдаём кнопке функции
+                                   
 
 /* #region Варинаты слов */
 var variant = [
@@ -57,14 +59,14 @@ var variant = [
 ]
 /* #endregion */ 
 
-
 /* #region Функция кнопочки */
-function handleButtonClick() {
+function handleButtonClick() {              // обработка нажатия на кнопку
     var textInput = document.getElementById("NickNameInput");
     var NickName = textInput.value;
-    if(NickName == ""){
+    if(NickName == ""){                     // проверка ника
         alert("Введите свой ник");
     }else{
+                                            // получаем нащи переменные
         var ul  =  document.getElementById("Retinglist");
         var li = document.createElement("li");
         var rand = Math.floor(Math.random()*variant.length);
@@ -72,11 +74,11 @@ function handleButtonClick() {
         console.log(rValue)
         
         
-        let result = games(rValue)
+        let result = games(rValue)          // играем и получаем результат
         
         
-        li.innerHTML = NickName + " " +  result;
-        ul.appendChild(li);
+        li.innerHTML = NickName + " " +  result;    
+        ul.appendChild(li);                 // добавляем участника в список лидеров
 
     }
 }
@@ -84,47 +86,58 @@ function handleButtonClick() {
 
 /* #region Сама игра */
 function games(strs) {
-    let counts = 5;
-    let strings = strs;
-    let hash = [];
+    let counts = 5;                         // количество промахов
+    let strings = strs;                     // сама строка
+    let hash = [];                          // массив с отгаданными символами
     for (let index = 0; index < strings.length; index++) {
-        hash[index] = "_";
-        
+        hash[index] = "_";                  // заполняем массив символами "_"
+
     }
 
-    while (counts > 0) {
+    while (counts > 0) {                    // основной цикл игры
         
-        let hash_str = hash.join()
+        
+
+
+
+        let hash_str = hash.join()          // делаем из массива $hash
+
+                                            // проверяем остался ли ещё неотгаданный символ
+        if(hash_str.indexOf("_") == -1) {
+            alert("Ты победил!")
+            return " угадал все буквы с " + (6 - counts) + " промхами"
+        }
         let dialog = "Слово состоит из " + strings.length + " букв." + "\n >> " + hash + 
         " \n Осталось " + ( counts) + " промахов"
         " \n\n Введите слово целиком или букву. ";
+                                            // текст диалога
+        otv = prompt(dialog)                // получаем символ или же слово
 
-        otv = prompt(dialog)
-        if (otv == null) {
+        if (otv == null) {                  // обрабатываем нажатие отмена
             return "Вышел из игры"
         }
-        if (otv == strings) {
+        if (otv == strings) {               // обработка введения слова полностью
             alert("Ты Угалад!")
-            return " победа с " + (6 - counts) + " раза"
+            return " победа с " + (6 - counts) + " промохами"
         }
-        if (otv.length == 1) {
+        if (otv.length == 1) {              // обрабрика ввода 1 симводла
             counts--;
-            if(strings.indexOf(otv) !== -1)
+            if(strings.indexOf(otv) !== -1)  // есть ли символ в строке
             {
                 hash[strings.indexOf(otv)] = strings[strings.indexOf(otv)] 
                 alert("Ты Угалад!")
                 counts++;
-            } else {
+            } else {                        // если его нет
                 alert("Такого символа нет. Осталось " + ( counts) + " промахов")
             }
         }
 
-        else {
+        else {                              // если что то ваще не понятное что 
             return " проиграл!"
         }
         
       }
-      alert("ты проиграл ")
+      alert(" ты проиграл")                 // если умудрился выйти каким то образом из этого цикла...
 
 
 
